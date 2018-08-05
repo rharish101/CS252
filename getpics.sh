04c864b4b49523d1712686bcfc610895cbd076de
rharish101 <harish.rajagopals@gmail.com>
2018-08-05 09:23:10 +0530

    Added APOD file modified for Gnome and Xfce

---
+++ getpics.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm -f ~/apod/*.jpg ~/apod/*.html
+if [ ! -d ~/apod ]; then
+    mkdir ~/apod
+fi
+cd ~/apod
+wget http://apod.nasa.gov/apod/astropix.html
+a='http://apod.nasa.gov/apod/'
+b=$(grep 'href="i' astropix.html | awk -F "\"" '{print $2}')
+wget $a$b
+imname=$(ls *.jpg)
+case $DESKTOP_SESSION in
+    "xfce")
+        xfconf-query --channel xfce4-desktop --property "/backdrop/screen0/monitor0/workspace0/last-image" --set "$(pwd)/$imname"
+        ;;
+    "ubuntu")
+        ;&
+    "gnome")
+        /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/$imname
+        ;;
+esac
+cd ~/Desktop
