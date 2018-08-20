f9ab60713f0c19fe414f382ee87d980e71b397f8
rharish101 <harish.rajagopals@gmail.com>
2018-08-20 15:55:15 +0530

    HTML problems and buffer problems fixed

--- client-server/client.c
+++ client-server/client.c
@@ -10,42 +10,42 @@
 #include <arpa/inet.h>
 
 int main(){
-  int clientSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  socklen_t addr_size;
-  char hello[1024];
-  scanf ("%[^\n]%*c", hello);
+    int clientSocket;
+    char buffer[1024];
+    struct sockaddr_in serverAddr;
+    socklen_t addr_size;
+    char hello[1024];
+    scanf ("%[^\n]%*c", hello);
 
 
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+    /*---- Create the socket. The three arguments are: ----*/
+    /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(5432);
-  /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    /*---- Configure settings of the server address struct ----*/
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+    /* Set port number, using htons function to use proper byte order */
+    serverAddr.sin_port = htons(5432);
+    /* Set IP address to localhost */
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  /*---- Connect the socket to the server using the address struct ----*/
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+    /*---- Connect the socket to the server using the address struct ----*/
+    addr_size = sizeof serverAddr;
+    connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
 
-  send(clientSocket , hello , strlen(hello) , 0 );
+    send(clientSocket , hello , strlen(hello) , 0 );
 
-  printf("Message sent\n");
+    printf("Message sent\n");
 
-  /*---- Read the message from the server into the buffer ----*/
-  recv(clientSocket, buffer, 1024, 0);
+    /*---- Read the message from the server into the buffer ----*/
+    recv(clientSocket, buffer, 1024, 0);
 
-  /*---- Print the received message ----*/
+    /*---- Print the received message ----*/
 
-  printf("Data received: %s",buffer);
+    printf("Data received: %s\n",buffer);
 
-  return 0;
+    return 0;
 }
--- client-server/server.c
+++ client-server/server.c
@@ -11,65 +11,84 @@
 
 
 int main(){
-  int welcomeSocket, newSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
+    int welcomeSocket, newSocket;
+    char buffer[1024];
+    struct sockaddr_in serverAddr;
+    struct sockaddr_storage serverStorage;
+    socklen_t addr_size;
 
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+    /*---- Create the socket. The three arguments are: ----*/
+    /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+    welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(5432);
-  /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    /*---- Configure settings of the server address struct ----*/
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+    /* Set port number, using htons function to use proper byte order */
+    serverAddr.sin_port = htons(5432);
+    /* Set IP address to localhost */
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  /*---- Bind the address struct to the socket ----*/
-  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    /*---- Bind the address struct to the socket ----*/
+    bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
-  /*---- Listen on the socket, with 5 max connection requests queued ----*/
-  while(1){
-      if(listen(welcomeSocket,5)==0)
+    /*---- Listen on the socket, with 5 max connection requests queued ----*/
+    if(listen(welcomeSocket,5)==0)
         printf("I'm listening\n");
-      else
+    else
         printf("Error\n");
 
-      /*---- Accept call creates a new socket for the incoming connection ----*/
-      addr_size = sizeof serverStorage;
-      newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+    while(1){
+        for (int i = 0; i < 1024; i++)
+            buffer[i] = '\0';
+        /*---- Accept call creates a new socket for the incoming connection ----*/
+        addr_size = sizeof serverStorage;
+        newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+        int valread = read(newSocket, buffer, 1024);
 
-      int valread = read( newSocket , buffer, 1024);
+        char html_code[6] = "html:";
+        char slice[6];
+        for (int i = 0; (buffer[i]) && (i < 5); i++)
+            slice[i] = buffer[i];
+        slice[5] = '\0';
+        if (strcmp(html_code, slice) != 0)
+        {
+            send(newSocket, buffer, 1024,0);
+            continue;
+        }
 
-      for(int i = 0; buffer[i]; i++){
-          buffer[i] = tolower(buffer[i]);
-      }
+        for (int i = 0; buffer[i + 5]; i++)
+            buffer[i] = buffer[i + 6];
 
-      printf("%s\n",buffer );
+        for(int i = 0; buffer[i]; i++)
+            buffer[i] = tolower(buffer[i]);
 
-      // READ file
-      char *file_contents;
-      long input_file_size;
-      FILE *input_file = fopen("demo.html", "rb");
-      fseek(input_file, 0, SEEK_END);
-      input_file_size = ftell(input_file);
-      rewind(input_file);
-      file_contents = malloc((input_file_size + 1) * (sizeof(char)));
-      fread(file_contents, sizeof(char), input_file_size, input_file);
-      fclose(input_file);
-      file_contents[input_file_size] = 0;
+        printf("%s\n",buffer);
+
+        // READ file
+        char *file_contents;
+        long input_file_size;
+        FILE *input_file = fopen(buffer, "rb");
+        if (!input_file)
+        {
+            send(newSocket, "Error 404", 1024, 0);
+            continue;
+        }
+        fseek(input_file, 0, SEEK_END);
+        input_file_size = ftell(input_file);
+        rewind(input_file);
+        file_contents = malloc((input_file_size + 1) * (sizeof(char)));
+        fread(file_contents, sizeof(char), input_file_size, input_file);
+        fclose(input_file);
+        file_contents[input_file_size] = 0;
 
 
-      /*---- Send message to the socket of the incoming connection ----*/
+        /*---- Send message to the socket of the incoming connection ----*/
 
-      send(newSocket,file_contents,input_file_size+1,0);
-  }
+        send(newSocket,file_contents,input_file_size+1,0);
+    }
 
-  return 0;
+    return 0;
 }
