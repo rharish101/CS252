7f14e6a566c515e89f3940c0d717e7735d18b4db
rharish101 <harish.rajagopals@gmail.com>
2018-09-02 09:48:16 +0530

    Client creates directory structure for images

--- assignment_1/client/client.c
+++ assignment_1/client/client.c
@@ -5,21 +5,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
 
+void store_images(int clientSocket, char *img_type, int count){
+    for(int i = 0; i < count; i++){
+        int img_size;
+        recv(clientSocket, &img_size, sizeof(img_size), 0);
+        printf("Image Size: %d %d\n", i, img_size);
+
+        //Read Picture Byte Array and convert to pic
+        char p_array[img_size];
+        recv(clientSocket, p_array, sizeof(p_array), 0);
+        FILE *image;
+        char dir_name[20];
+        sprintf(dir_name, "images/%s", img_type);
+        char img_name[25];
+        sprintf(img_name, "images/%s/%d.jpg", img_type, i + 1);
+
+        mkdir("images", 0755);
+        mkdir(dir_name, 0755);
+        image = fopen(img_name,"w");
+
+        fwrite(p_array, 1, sizeof(p_array), image);
+        fclose(image);
+    }
+}
+
 int main(){
     int clientSocket;
-    char buffer[1024];
     struct sockaddr_in serverAddr;
     socklen_t addr_size;
     char hello[300];
 
     scanf ("%[^\n]%*c", hello);
 
-    printf("Message sent : %s\n", hello);
+    printf("Message to send : %s\n", hello);
 
     /*---- Create the socket. The three arguments are: ----*/
     /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
@@ -44,121 +68,23 @@ int main(){
     printf("Message sent : %s\n", hello);
 
     /*---- Read the message from the server into the buffer ----*/
-    int dog_count;
-    recv(clientSocket, &dog_count, sizeof(dog_count), 0);
-    printf("Dog count : %d\n",dog_count );
+    int count;
 
-    for(int i=0;i < dog_count;i++){
-        int img_size;
-        recv(clientSocket, &img_size, sizeof(img_size), 0);
-        printf("Image Size: %d %d\n",i, img_size );
+    recv(clientSocket, &count, sizeof(count), 0);
+    printf("Dog count : %d\n", count);
+    store_images(clientSocket, "dogs", count);
 
-        //Read Picture Byte Array and convert to pic
-        char p_array[img_size];
-        recv(clientSocket, p_array, sizeof(p_array), 0);
-        FILE *image;
-        switch(i){
-            case 0: image = fopen("dog1.jpg","w");
-                   break;
-            case 1: image = fopen("dog2.jpg","w");
-                   break;
-            case 2: image = fopen("dog3.jpg","w");
-                   break;
-            case 3: image = fopen("dog4.jpg","w");
-                   break;
-        }
+    recv(clientSocket, &count, sizeof(count), 0);
+    printf("cat count : %d\n",count );
+    store_images(clientSocket, "cats", count);
 
-        fwrite(p_array, 1, sizeof(p_array), image);
-        fclose(image);
+    recv(clientSocket, &count, sizeof(count), 0);
+    printf("car count : %d\n",count );
+    store_images(clientSocket, "cars", count);
 
-    }
-
-    int cat_count;
-    recv(clientSocket, &cat_count, sizeof(cat_count), 0);
-    printf("cat count : %d\n",cat_count );
-
-    for(int i=0;i < cat_count;i++){
-        int img_size;
-        recv(clientSocket, &img_size, sizeof(img_size), 0);
-        printf("Image Size: %d %d\n",i, img_size );
-
-        //Read Picture Byte Array and convert to pic
-        char p_array[img_size];
-        recv(clientSocket, p_array, sizeof(p_array), 0);
-        FILE *image;
-        switch(i){
-            case 0: image = fopen("cat1.jpg","w");
-                   break;
-            case 1: image = fopen("cat2.jpg","w");
-                   break;
-            case 2: image = fopen("cat3.jpg","w");
-                   break;
-            case 3: image = fopen("cat4.jpg","w");
-                   break;
-        }
-
-        fwrite(p_array, 1, sizeof(p_array), image);
-        fclose(image);
-
-    }
-
-    int car_count;
-    recv(clientSocket, &car_count, sizeof(car_count), 0);
-    printf("car count : %d\n",car_count );
-
-    for(int i=0;i < car_count;i++){
-        int img_size;
-        recv(clientSocket, &img_size, sizeof(img_size), 0);
-        printf("Image Size: %d %d\n",i, img_size );
-
-        //Read Picture Byte Array and convert to pic
-        char p_array[img_size];
-        recv(clientSocket, p_array, sizeof(p_array), 0);
-        FILE *image;
-        switch(i){
-            case 0: image = fopen("car1.jpg","w");
-                   break;
-            case 1: image = fopen("car2.jpg","w");
-                   break;
-            case 2: image = fopen("car3.jpg","w");
-                   break;
-            case 3: image = fopen("car4.jpg","w");
-                   break;
-        }
-
-        fwrite(p_array, 1, sizeof(p_array), image);
-        fclose(image);
-
-    }
-
-    int truck_count;
-    recv(clientSocket, &truck_count, sizeof(truck_count), 0);
-    printf("truck count : %d\n",truck_count );
-
-    for(int i=0;i < truck_count;i++){
-        int img_size;
-        recv(clientSocket, &img_size, sizeof(img_size), 0);
-        printf("Image Size: %d %d\n",i, img_size );
-
-        //Read Picture Byte Array and convert to pic
-        char p_array[img_size];
-        recv(clientSocket, p_array, sizeof(p_array), 0);
-        FILE *image;
-        switch(i){
-            case 0: image = fopen("truck1.jpg","w");
-                   break;
-            case 1: image = fopen("truck2.jpg","w");
-                   break;
-            case 2: image = fopen("truck3.jpg","w");
-                   break;
-            case 3: image = fopen("truck4.jpg","w");
-                   break;
-        }
-
-        fwrite(p_array, 1, sizeof(p_array), image);
-        fclose(image);
-
-    }
+    recv(clientSocket, &count, sizeof(count), 0);
+    printf("truck count : %d\n",count );
+    store_images(clientSocket, "trucks", count);
 
     int html_size;
     recv(clientSocket, &html_size, sizeof(html_size), 0);
@@ -170,9 +96,7 @@ int main(){
     FILE *html = fopen("output.html","w") ;
     fwrite(p_array, 1, sizeof(p_array), html);
     fclose(html);
-
     popen("xdg-open ./output.html", "r");
-
 
     return 0;
 }
