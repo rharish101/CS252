b5ba648f48fbb54e6c4ba16bd527728d911c0b8f
rharish101 <harish.rajagopals@gmail.com>
2018-09-02 09:08:31 +0530

    Cleaned directories and extra files

--- assignment_1/server/scripts/img_getter.sh
+++ assignment_1/server/scripts/img_getter.sh
@@ -6,7 +6,7 @@ arr=("car" "truck" "cat" "dog")
 count=0
 for num in $(./scripts/parser.sh "$request"); do
     while (( $num > 0 )); do
-        echo "./${arr[$count]}$num.jpg"
+        echo "images/${arr[$count]}s/$num.jpg"
         num=$((num-1))
     done
     count=$((count+1))
