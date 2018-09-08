e2fe90eb08f4652e2bf290b7236c6bcc8e589752
wathore <wathore@iitk.ac.in>
2018-09-08 15:33:57 +0530

    Add files via upload

---
+++ website/id.php
@@ -0,0 +1,64 @@
+<html>
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Shubharshu@143";
+$dbname = "employees";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$em_id=$_POST['employ_id'];
+$query = "SELECT `birth_date`, `first_name`, `last_name` , `gender` , `hire_date` FROM `employees` WHERE `emp_no`= '$em_id'";
+	$res = mysqli_query( $conn,$query )
+	or die("failed to query in database".mysqli_error($conn));
+$row = mysqli_fetch_array($res);
+
+echo "First name: ";
+echo $row['first_name'];
+echo "</br>";
+
+echo "Last name: ";
+echo $row['last_name'];
+echo "</br>";
+
+echo "Gender: ";
+echo $row['gender'];
+echo "</br>";
+
+
+echo "Birth date: ";
+echo $row['birth_date'];
+echo "</br>";
+
+
+$query = "SELECT `dept_no`, `from_date`, `to_date`  FROM `dept_emp` WHERE `emp_no`= '$em_id'";
+	$res = mysqli_query( $conn,$query )
+	or die("failed to query in database".mysqli_error($conn));
+$row = mysqli_fetch_array($res);
+
+
+echo "Department Number: ";
+echo $row['dept_no'];
+echo "</br>";
+
+echo "From: ";
+echo $row['from_date'];
+echo "</br>";
+
+echo "To Date ";
+echo $row['to_date'];
+echo "</br>";
+
+
+?>
+
+
+
+</html>
---
+++ website/index.php
@@ -0,0 +1,43 @@
+<html>
+
+<form name="form1" method="post" action="id.php">
+
+
+  
+      
+      Employee id:
+          <input type="number" name="employ_id" placeholder="type employee id...." />
+      <br>
+      
+      
+      <input type="submit" name="submit" value="search using employee id"/>
+      <br>
+  
+
+
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
