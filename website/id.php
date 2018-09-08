6497e188fb98440fd14f8babf59415a2eb743396
rharish101 <harish.rajagopals@gmail.com>
2018-09-08 15:56:21 +0530

    Added functionality to use username and password for db and report error on failed fetch

--- website/id.php
+++ website/id.php
@@ -1,17 +1,15 @@
 <html>
-
-
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "Shubharshu@143";
+$username = $_POST['username'];
+$password = $_POST['password'];
 $dbname = "employees";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
 $em_id=$_POST['employ_id'];
@@ -19,6 +17,10 @@ $query = "SELECT `birth_date`, `first_name`, `last_name` , `gender` , `hire_date
 	$res = mysqli_query( $conn,$query )
 	or die("failed to query in database".mysqli_error($conn));
 $row = mysqli_fetch_array($res);
+
+if ($row['first_name'] === NULL) {
+  die("no such employee exists");
+}
 
 echo "First name: ";
 echo $row['first_name'];
@@ -39,8 +41,8 @@ echo "</br>";
 
 
 $query = "SELECT `dept_no`, `from_date`, `to_date`  FROM `dept_emp` WHERE `emp_no`= '$em_id'";
-	$res = mysqli_query( $conn,$query )
-	or die("failed to query in database".mysqli_error($conn));
+  $res = mysqli_query( $conn,$query )
+  or die("failed to query in database".mysqli_error($conn));
 $row = mysqli_fetch_array($res);
 
 
@@ -55,10 +57,5 @@ echo "</br>";
 echo "To Date ";
 echo $row['to_date'];
 echo "</br>";
-
-
 ?>
-
-
-
 </html>
