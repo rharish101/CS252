b6561dcfaf49ea34b0074c01a78593f529192d64
rharish101 <harish.rajagopals@gmail.com>
2018-09-08 15:51:11 +0530

    Added functionality to get username and password for db

--- website/index.php
+++ website/index.php
@@ -1,5 +1,11 @@
 <html>
 <form name="form1" method="post" action="id.php">
+  DB Username:
+    <input type="string" name="username" placeholder="type db username...." />
+  <br>
+  DB Password:
+    <input type="password" name="password" placeholder="type db password...." />
+  <br>
   Employee id:
     <input type="number" name="employ_id" placeholder="type employee id...." />
   <br>
