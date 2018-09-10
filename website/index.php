60fc5096ab6518574ca177c63852f8e240852a87
rharish101 <harish.rajagopals@gmail.com>
2018-09-10 14:57:06 +0530

    Switched to mongodb

--- website/index.php
+++ website/index.php
@@ -1,18 +1,16 @@
 <html>
-<form name="form1" method="post" action="id.php">
-  DB Username:
-    <input type="string" name="username" placeholder="type db username...." />
-  <br>
-  DB Password:
-    <input type="password" name="password" placeholder="type db password...." />
-  <br>
-  Employee id:
-    <input type="number" name="employ_id" placeholder="type employee id...." />
-  <br>
-  Employee last name:
-    <input type="text" name="lastname" placeholder="type last name" />
-  <br>
-  <input type="submit" name="submit" value="search"/>
-  <br>
-</form>
+  <head>
+    <title>MongoDB CS252</title>
+  </head>
+  <body>
+    <?php
+      require 'vendor/autoload.php';
+      $client = new MongoDB\Client("mongodb://localhost:27017");
+      $collection = $client->cases->cases;
+      $result = $collection->find();
+      foreach ($result as $item) {
+        echo $item['DISTRICT']."<br>";
+      }
+    ?>
+  </body>
 </html>
---
+++ website/index_sql.php
@@ -0,0 +1,18 @@
+<html>
+<form name="form1" method="post" action="id.php">
+  DB Username:
+    <input type="string" name="username" placeholder="type db username...." />
+  <br>
+  DB Password:
+    <input type="password" name="password" placeholder="type db password...." />
+  <br>
+  Employee id:
+    <input type="number" name="employ_id" placeholder="type employee id...." />
+  <br>
+  Employee last name:
+    <input type="text" name="lastname" placeholder="type last name" />
+  <br>
+  <input type="submit" name="submit" value="search"/>
+  <br>
+</form>
+</html>
