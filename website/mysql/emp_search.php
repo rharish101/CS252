ad1dc5d7f46ed7d4c0bb51c3528e03ae5afdcba0
rharish101 <harish.rajagopals@gmail.com>
2018-09-21 13:52:05 +0530

    Changed checking of multiple field from PHP commands to MySQL query

--- website/mysql/emp_search.php
+++ website/mysql/emp_search.php
@@ -1,175 +1,66 @@
 <?php
-  function query_dept($conn, $dept_name)
-  {
-    // Get department number
-    $query1 = "SELECT * FROM departments WHERE dept_name = '" . $dept_name . "'";
-    $res1 = mysqli_query($conn, $query1)
-      or die("Failed to query in database: " . mysqli_error($conn));
-    $row1 = mysqli_fetch_array($res1);
-    if ($row1['dept_no'] === NULL)
-    {
-      die("No such employee exists");
-    }
-
-    // Get department details
-    $query2 = "SELECT * FROM dept_emp WHERE dept_no = '" . $row1['dept_no'] . "'";
-    $res2 = mysqli_query($conn, $query2)
-      or die("Failed to query in database: " . mysqli_error($conn));
-
-    $row2 = mysqli_fetch_array($res2);
-    if ($row2['emp_no'] === NULL)
-    {
-      die("No such employee exists");
-    }
-
-    while ($row2 !== NULL)
-    {
-      // Get employee details
-      $query3 = "SELECT * FROM employees WHERE emp_no = '" . $row2['emp_no'] . "'";
-      $res3 = mysqli_query($conn, $query3)
-        or die("Failed to query in database: " . mysqli_error($conn));
-      $row3 = mysqli_fetch_array($res3);
-
-      echo "First name: ";
-      echo $row3['first_name'];
-      echo "</br>";
-
-      echo "Last name: ";
-      echo $row3['last_name'];
-      echo "</br>";
-
-      echo "Employee ID: ";
-      echo $row2['emp_no'];
-      echo "</br>";
-
-      echo "Gender: ";
-      echo $row3['gender'];
-      echo "</br>";
-
-      echo "Birth date: ";
-      echo $row3['birth_date'];
-      echo "</br>";
-
-      echo "Department: ";
-      echo $dept_name;
-      echo "</br>";
-
-      echo "From: ";
-      echo $row2['from_date'];
-      echo "</br>";
-
-      echo "To Date ";
-      echo $row2['to_date'];
-      echo "</br>";
-
-      $row2 = mysqli_fetch_array($res2);
-      echo "</br>";
-    }
-  }
-
   function fetch_emp_data ($conn, $emp_no, $last_name, $dept_name)
   {
-    $query1 = "SELECT * FROM employees WHERE ";
+    $query = "SELECT employees.*, dept_emp.*, departments.* FROM employees, dept_emp, departments WHERE (employees.emp_no = dept_emp.emp_no) AND (dept_emp.dept_no = departments.dept_no)";
     if ($emp_no !== "")
     {
-      $query1 = $query1 . "emp_no = $emp_no";
+      $query = $query . "AND (employees.emp_no = $emp_no)";
     }
     if ($last_name !== "")
     {
-      if ($emp_no !== "")
-      {
-        $query1 = $query1 . " AND ";
-      }
-      $query1 = $query1 . "last_name = '$last_name'";
+      $query = $query . "AND (employees.last_name = '$last_name')";
     }
-
-    // Search by department instead of employees
-    if (($emp_no === "") && ($last_name === ""))
+    if ($dept_name !== "")
     {
-      if ($dept_name === "")
-      {
-        die("No data given");
-      }
-      else
-      {
-        query_dept($conn, $dept_name);
-        return;
-      }
+      $query = $query . "AND (departments.dept_name = '$dept_name')";
     }
 
     // Get employee details
-    $res1 = mysqli_query($conn, $query1)
+    $res = mysqli_query($conn, $query)
       or die("Failed to query in database: " . mysqli_error($conn));
-    $row1 = mysqli_fetch_array($res1);
-    if ($row1['first_name'] === NULL)
+    $row = mysqli_fetch_array($res);
+    if ($row['first_name'] === NULL)
     {
       die("No such employee exists");
     }
 
     $found = FALSE;
-    while ($row1 !== NULL)
+    while ($row !== NULL)
     {
-      // Get department details
-      $query2 = "SELECT * FROM `dept_emp` WHERE emp_no = " . $row1['emp_no'];
-      $res2 = mysqli_query($conn, $query2)
-        or die("failed to query in database: " . mysqli_error($conn));
-      $row2 = mysqli_fetch_array($res2);
-
-      // Get department name
-      $query3 = "SELECT * FROM `departments` WHERE dept_no = '" . $row2['dept_no'] . "'";
-      $res3 = mysqli_query($conn, $query3)
-        or die("failed to query in database: " . mysqli_error($conn));
-      $row3 = mysqli_fetch_array($res3);
-
-      if (($dept_name !== "") && (strcasecmp($row3['dept_name'], $dept_name) != 0))
-      {
-        $row1 = mysqli_fetch_array($res1);
-        continue;
-      }
-      else
-      {
-        $found = TRUE;
-      }
-
       echo "First name: ";
-      echo $row1['first_name'];
+      echo $row['first_name'];
       echo "</br>";
 
       echo "Last name: ";
-      echo $row1['last_name'];
+      echo $row['last_name'];
       echo "</br>";
 
       echo "Employee ID: ";
-      echo $row1['emp_no'];
+      echo $row['emp_no'];
       echo "</br>";
 
       echo "Gender: ";
-      echo $row1['gender'];
+      echo $row['gender'];
       echo "</br>";
 
       echo "Birth date: ";
-      echo $row1['birth_date'];
+      echo $row['birth_date'];
       echo "</br>";
 
       echo "Department: ";
-      echo $row3['dept_name'];
+      echo $row['dept_name'];
       echo "</br>";
 
       echo "From: ";
-      echo $row2['from_date'];
+      echo $row['from_date'];
       echo "</br>";
 
       echo "To Date ";
-      echo $row2['to_date'];
+      echo $row['to_date'];
       echo "</br>";
 
-      $row1 = mysqli_fetch_array($res1);
+      $row = mysqli_fetch_array($res);
       echo "</br>";
-    }
-
-    if ($found == FALSE)
-    {
-      die("No such employee exists");
     }
   }
 ?>
