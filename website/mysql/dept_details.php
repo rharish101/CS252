4343ddddd2747fee0b21600aec2623cce6cf5707
rharish101 <harish.rajagopals@gmail.com>
2018-09-24 19:13:57 +0530

    Now creating custom tables in MySQL for faster calculation of pay ratio

--- website/mysql/dept_details.php
+++ website/mysql/dept_details.php
@@ -45,43 +45,50 @@
 
   function get_pay_ratio($conn, $dept_name)
   {
-    $query = "SELECT title, COALESCE(M, 0) AS M, COALESCE(F, 0) AS F, cnt
-              FROM
-              (
-                SELECT title, SUM(M) AS M, SUM(F) AS F, SUM(cnt) as cnt
-                FROM
-                (
-                  SELECT
-                    mytitles.title,
-                    CASE WHEN employees.gender = 'M' THEN AVG(mysalaries.salary) END AS M,
-                    CASE WHEN employees.gender = 'F' THEN AVG(mysalaries.salary) END AS F,
-                    COUNT(employees.emp_no) AS cnt
-                  FROM employees
-                  INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-                  INNER JOIN
-                  (
-                    SELECT emp_no, MAX(salary) AS salary
-                    FROM salaries
-                    GROUP BY emp_no
-                  ) AS mysalaries ON employees.emp_no = mysalaries.emp_no
-                  INNER JOIN
-                  (
-                    SELECT t1.emp_no, t1.title
-                    FROM titles AS t1
-                    INNER JOIN
-                    (
-                      SELECT MAX(from_date) AS maxm, emp_no
-                      FROM titles
-                      GROUP BY titles.emp_no
-                    ) AS t2 ON t1.emp_no = t2.emp_no AND t1.from_date = t2.maxm
-                  ) AS mytitles ON employees.emp_no = mytitles.emp_no
-                  INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
-                  WHERE departments.dept_name = '". $dept_name . "'
-                  GROUP BY mytitles.title, employees.gender
-                ) AS pay_ratio_nulls
-                GROUP BY title
-              ) AS pay_ratio";
-    $res = mysqli_query($conn, $query)
+    $query1 = "CREATE TABLE IF NOT EXISTS mysalaries
+               (PRIMARY KEY(emp_no))
+               SELECT emp_no, MAX(salary) AS salary
+               FROM salaries
+               GROUP BY emp_no";
+
+    $query2 = "CREATE TABLE IF NOT EXISTS mytitles
+               (PRIMARY KEY(emp_no))
+               SELECT t1.emp_no, t1.title
+               FROM titles AS t1
+               INNER JOIN
+               (
+                   SELECT MAX(from_date) AS maxm, emp_no
+                   FROM titles
+                   GROUP BY titles.emp_no
+               ) AS t2 ON t1.emp_no = t2.emp_no AND t1.from_date = t2.maxm;";
+
+    $query3 = "SELECT title, COALESCE(M, 0) AS M, COALESCE(F, 0) AS F, cnt
+               FROM
+               (
+                 SELECT title, SUM(M) AS M, SUM(F) AS F, SUM(cnt) as cnt
+                 FROM
+                 (
+                   SELECT
+                     mytitles.title,
+                     CASE WHEN employees.gender = 'M' THEN AVG(mysalaries.salary) END AS M,
+                     CASE WHEN employees.gender = 'F' THEN AVG(mysalaries.salary) END AS F,
+                     COUNT(employees.emp_no) AS cnt
+                   FROM employees
+                   INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+                   INNER JOIN mysalaries ON employees.emp_no = mysalaries.emp_no
+                   INNER JOIN mytitles ON employees.emp_no = mytitles.emp_no
+                   INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+                   WHERE departments.dept_name = '". $dept_name . "'
+                   GROUP BY mytitles.title, employees.gender
+                 ) AS pay_ratio_nulls
+                 GROUP BY title
+               ) AS pay_ratio;";
+
+    mysqli_query($conn, $query1)
+      or die("Failed to query in database: " . mysqli_error($conn));
+    mysqli_query($conn, $query2)
+      or die("Failed to query in database: " . mysqli_error($conn));
+    $res = mysqli_query($conn, $query3)
       or die("Failed to query in database: " . mysqli_error($conn));
     $row = mysqli_fetch_array($res);
     if ($row['title'] === NULL)
