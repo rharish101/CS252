2a48d192f4149e3e756562150b500e7a58c92f6d
Ashish Kumar <ak.ashish2108@gmail.com>
2018-09-29 17:07:51 +0530

    Updated the search and migrated db details in a file

--- website/mysql/handle_req.php
+++ website/mysql/handle_req.php
@@ -3,13 +3,9 @@
     include 'emp_search.php';
     include 'dept_details.php';
 
-    $servername = "localhost";
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $dbname = "employees";
-
+    $database = include('config.php');
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($database['host'], $database['user'], $database['pass'], $database['name']);
     // Check connection
     if ($conn->connect_error)
     {
@@ -24,7 +20,7 @@
         fetch_emp_data($conn, $_POST['employ_id'], $_POST['lastname'], $_POST['dept_name']);
       }
       else
-        die("Incorrect inputs");
+        die("Incorrect inputs For employee search");
     }
 
     elseif (isset($_POST['larg_dept']))
@@ -41,7 +37,27 @@
         fetch_dept_data($conn, $_POST['department']);
       }
       else
-        die("Incorrect inputs");
+        die("Incorrect Department Name");
+    }
+    elseif (isset($_POST['gr_dept_det']))
+    {
+      if (preg_match("/^[A-Za-z]+$/", $_POST['gr_department']))
+      {
+        $gr = get_gender_ratio($conn, $_POST['gr_department']);
+       echo "Gender ratio (females/males) in  " .  $_POST['gr_department'] . " Department = " . $gr ;
+      }
+      else
+        die("Incorrect Department Name");
+    }
+    elseif (isset($_POST['gpr_dept_det']))
+    {
+      if (preg_match("/^[A-Za-z]+$/", $_POST['gpr_department']))
+      {
+        $gpr = get_pay_ratio($conn, $_POST['gpr_department']);
+        echo "Gender Pay ratio (females/males) in  " .  $_POST['gr_department'] . " Department = " . $gpr ;
+      }
+      else
+        die("Incorrect Department Name");
     }
   ?>
 </html>
