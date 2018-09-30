19f73d2e5404b431305a27ca01b3570fef1e842e
rharish101 <harish.rajagopals@gmail.com>
2018-09-30 17:09:55 +0530

    Now title is set dynamically

--- assignment_2/mysql/handle_req.php
+++ assignment_2/mysql/handle_req.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-</head>
-<body>
   <?php
     include 'emp_search.php';
     include 'dept_details.php';
@@ -23,6 +14,21 @@
       die("Connection failed: " . $conn->connect_error);
     }
 
+    if (isset($_POST['emp_search']))
+      echo "<title>Employee Search</title>";
+    else
+      echo "<title>Department Details</title>";
+  ?>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <?php
     if (isset($_POST['emp_search']))
     {
       if (preg_match("/^[A-Za-z]*$/", $_POST['lastname']) && preg_match("/^[A-Za-z]*$/", $_POST['dept_name']))
