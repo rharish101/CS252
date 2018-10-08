f9c6eaffb4608c7388f22a815cb027995d645e2c
rharish101 <harish.rajagopals@gmail.com>
2018-10-08 20:11:32 +0530

    Added functionality to save driver details and delete on demand; notifications area for drivers is now only visible on entering details

--- NFCL/src/pages/driver/driver.ts
+++ NFCL/src/pages/driver/driver.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Http } from '@angular/http';
 import { GlobalVarsService } from '../../services/globalvars/globalvars';
+import { Storage } from '@ionic/storage';
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
@@ -23,71 +24,112 @@ import 'rxjs/add/operator/map';
 })
 
 export class DriverPage {
-  constructor(private geolocation: Geolocation, private statusBar: StatusBar, public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public http: Http, public globalvars:GlobalVarsService) { }
+  constructor(private storage: Storage, private geolocation: Geolocation, private statusBar: StatusBar, public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public http: Http, public globalvars:GlobalVarsService) { }
 
   ionViewDidLoad() {
     this.statusBar.overlaysWebView(false);
     this.statusBar.backgroundColorByHexString('#636b80');
-    const prompt = this.alertCtrl.create({
-      title: 'Details',
-      message: "Enter your details",
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Name',
-          type: 'text'
-        },
-        {
-          name: 'phone',
-          placeholder: "Phone Number",
-          type: 'tel'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel');
-            this.navCtrl.popToRoot();
-          }
-        },
-        {
-          text: 'Save',
-          handler: data => {
-            console.log(data);
-            this.handleData(data);
-            console.log('Saved');
-          }
-        }
-      ],
-      cssClass: 'alertCustomCss',
-      enableBackdropDismiss: false
+    this.storage.get('driverdetails').then((val) => {
+      if (val === null) {
+        this.storage.set('driverdetails', 'exists');
+        const prompt = this.alertCtrl.create({
+          title: 'Details',
+          message: "Enter your details",
+          inputs: [
+            {
+              name: 'name',
+              placeholder: 'Name',
+              type: 'text'
+            },
+            {
+              name: 'phone',
+              placeholder: "Phone Number",
+              type: 'tel'
+            }
+          ],
+          buttons: [
+            {
+              text: 'Cancel',
+              handler: data => {
+                console.log('Cancel');
+                this.navCtrl.popToRoot();
+              }
+            },
+            {
+              text: 'Save',
+              handler: data => {
+                console.log(data);
+                this.handleData(data);
+                console.log('Saved');
+              }
+            }
+          ],
+          cssClass: 'alertCustomCss',
+          enableBackdropDismiss: false
+        });
+        prompt.present();
+      }
+      else {
+        this.notifVisible = true;
+        this.updateLocation();
+      }
     });
-    prompt.present();
     console.log('ionViewDidLoad DriverPage');
   }
 
-  ionViewDidLeave() {
-    this.http.post(this.server, {
-      name: this.name,
-      phone: this.phone,
-      latitude: 0,
-      longitude: 0,
-      remove: true
-    }).map(res => res.json()).subscribe((data) => {
-      console.log('Data delete successful', data);
-    }, (error) => {
-      console.log('Server error', error);
+  deleteDetails() {
+    this.storage.get('drivercontacts').then((val) => {
+      this.http.post(this.server, {
+        name: val.name,
+        phone: val.phone,
+        registration: this.globalvars.registrationId,
+        latitude: 0,
+        longitude: 0,
+        remove: true
+      }).map(res => res.json()).subscribe((data) => {
+        this.storage.set('driverdetails', null);
+        this.storage.set('drivercontacts', null);
+        console.log('Data delete successful', data);
+        this.popup = this.alertCtrl.create({
+          title: 'Success',
+          message: 'Data successfully deleted',
+          buttons: [
+            {
+              text: 'Exit',
+              handler: data => {
+                this.navCtrl.popToRoot();
+              }
+            }
+          ],
+          cssClass: 'alertCustomCss',
+          enableBackdropDismiss: false
+        });
+        this.popup.present();
+      }, (error) => {
+        console.log('Server error', error);
+        try {
+          this.popup.dismiss();
+        } catch(e) {
+          console.log("try-catch", e);
+        }
+        this.popup = this.alertCtrl.create({
+          title: 'Server Error',
+          message: 'Please try again',
+          buttons: ['OK'],
+          cssClass: 'alertCustomCss',
+        });
+        this.popup.present();
+      });
+      this.onDestroy$.next();
     });
-    this.onDestroy$.next();
   }
 
   private server: string = "https://cse.iitk.ac.in/users/rharish/NFCL/update.php";
-  private name: string = "";
-  private phone: number = 0;
+
   // private latitude: number = 0;
   // private longitude: number = 0;
 
+  private notifVisible: boolean = false;
   private popup;
   private onDestroy$ = new Subject<void>();
 
@@ -97,9 +139,52 @@ export class DriverPage {
       // this.longitude = resp.coords.longitude;
       console.log(resp.coords.latitude, resp.coords.longitude);
 
+      this.storage.get('drivercontacts').then((val) => {
+        this.http.post(this.server, {
+          name: val.name,
+          phone: val.phone,
+          registration: this.globalvars.registrationId,
+          latitude: resp.coords.latitude,
+          longitude: resp.coords.longitude,
+          remove: false
+        }).map(res => res.json()).subscribe((data) => {
+          console.log('Location update successful', data);
+        }, (error) => {
+          console.log('Server error', error);
+          try {
+            this.popup.dismiss();
+          } catch(e) {
+            console.log("try-catch", e);
+          }
+          this.popup = this.alertCtrl.create({
+            title: 'Server Error',
+            message: 'Please try again',
+            buttons: ['OK'],
+            cssClass: 'alertCustomCss',
+          });
+          this.popup.present();
+        });
+      });
+    }).catch((error) => {
+      console.log('Error in location', error);
+      try {
+        this.popup.dismiss();
+      } catch(e) {
+        console.log("try-catch", e);
+      }
+      this.popup = this.alertCtrl.create({
+        title: 'Error',
+        message: "Could not retrieve location. Please try again",
+        buttons: ['Ok'],
+        cssClass: 'alertCustomCss',
+      });
+      this.popup.present();
+    });
+
+    /* this.storage.get('drivercontacts').then((val) => {
       this.http.post(this.server, {
-        name: this.name,
-        phone: this.phone,
+        name: val.name,
+        phone: val.phone,
         registration: this.globalvars.registrationId,
         latitude: resp.coords.latitude,
         longitude: resp.coords.longitude,
@@ -118,49 +203,9 @@ export class DriverPage {
           message: 'Please try again',
           buttons: ['OK'],
           cssClass: 'alertCustomCss',
-          enableBackdropDismiss: false
         });
         this.popup.present();
       });
-    }).catch((error) => {
-      console.log('Error in location', error);
-      try {
-        this.popup.dismiss();
-      } catch(e) {
-        console.log("try-catch", e);
-      }
-      this.popup = this.alertCtrl.create({
-        title: 'Error',
-        message: "Could not retrieve location. Please try again",
-        buttons: ['Ok'],
-        cssClass: 'alertCustomCss',
-      });
-      this.popup.present();
-    });
-
-    /* this.http.post(this.server, {
-      name: this.name,
-      phone: this.phone,
-      latitude: this.latitude,
-      longitude: this.longitude,
-      remove: false
-    }).map(res => res.json()).subscribe((data) => {
-      console.log('Location update successful', data);
-    }, (error) => {
-      console.log('Server error', error);
-      try {
-        this.popup.dismiss();
-      } catch(e) {
-        console.log("try-catch", e);
-      }
-      this.popup = this.alertCtrl.create({
-        title: 'Server Error',
-        message: 'Please try again',
-        buttons: ['OK'],
-        cssClass: 'alertCustomCss',
-        enableBackdropDismiss: false
-      });
-      this.popup.present();
     }); */
   }
 
@@ -183,12 +228,12 @@ export class DriverPage {
       error.present();
     }
     else {
-      this.name = data['name'];
-      this.phone = Number(data['phone']);
+      this.storage.set('drivercontacts', {"name": data['name'], "phone": Number(data['phone'])});
       this.updateLocation();
-      Observable.interval(1000 * 5).takeUntil(this.onDestroy$).subscribe(x => {
+      Observable.interval(1000 * 60 * 20).takeUntil(this.onDestroy$).subscribe(x => {
         this.updateLocation();
       });
+      this.notifVisible = true;
     }
   }
 }
