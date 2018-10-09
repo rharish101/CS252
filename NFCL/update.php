0c609aed28bdd221a0cfc158f8f27bec57ae5aed
rharish101 <harish.rajagopals@gmail.com>
2018-10-09 14:34:04 +0530

    Added updated server php file for drivers

--- NFCL/update.php
+++ NFCL/update.php
@@ -1,6 +1,6 @@
 <?php
   $_POST = json_decode(file_get_contents('php://input'), true);
-  $data = '{"name": "' . $_POST["name"] . '", "phone": ' . $_POST["phone"] .  ', "latitude": ' . $_POST["latitude"] . ', "longitude": ' . $_POST["latitude"] . ', "remove": ' . (($_POST["remove"]) ? 'true' : 'false') . '}';
-  $log = file_put_contents('/home/rharish/public_html/logs.txt', $data.PHP_EOL, FILE_APPEND | LOCK_EX);
+  $data = '{"name": "' . $_POST["name"] . '", "phone": ' . $_POST["phone"] . ', "registration": "' . $_POST["registration"] . '", "latitude": ' . $_POST["latitude"] . ', "longitude": ' . $_POST["latitude"] . ', "remove": ' . (($_POST["remove"]) ? 'true' : 'false') . '}';
+  $log = file_put_contents('logs.txt', $data.PHP_EOL, FILE_APPEND | LOCK_EX);
   echo $data;
 ?>
