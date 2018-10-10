8d1401cb5f7c47e34f5f93e1662097a6152e6068
Ashish Kumar <ak.ashish2108@gmail.com>
2018-10-11 01:56:25 +0530

    added update view and pyproj

--- NFCL/server/core/views.py
+++ NFCL/server/core/views.py
@@ -8,6 +8,51 @@ from pyproj import Proj, transform
 
 
 
+@require_http_methods(["POST"])
+def updateDriverDetail(request):
+
+    data = json.loads(request.body.decode("utf-8"))
+    
+    try:
+        mob_id = data['mob_id']
+        name = data['name']
+        mobile_no = data['mobile_no']
+        latitude = data['latitude']
+        longitude = data['longitude']
+
+    except:
+         return HttpResponse("ERROR", status=403)
+
+    x, y = transform(Proj(init='epsg:4326'), Proj(init='epsg:3857'), float(longitude), float(latitude))
+    # output (meters east of 0, meters north of 0):
+    #shortcuts for Web Mercator (EPSG 3857) and WGS 84 longitude and latitude (EPSG 4326). 
+    x = int(x)
+    y = int(y)
+
+    grid_size = 10000 # 10km * 10km
+    grid = ( int(x/grid_size), int(y/grid_size))
+    grid_id = str(hash(grid))
+
+    data_update = {
+        'mob_id' : mob_id,
+        'name' : name,
+        'mobile_no' : mobile_no,
+        'latitude' : latitude,
+        'longitude' : longitude,
+        'x_cordinate' : x,
+        'y_cordinate' : y,
+        'grid' : grid_id
+    }
+
+    try:
+        driver = Driver.objects.get(mob_id = mob_id)
+        Driver.objects.filter(mob_id = mob_id).update(**data_update)
+        return HttpResponse('UPDATE', status = 200)
+    except ObjectDoesNotExist:
+        driver = Driver(**data_update)
+        driver.save()
+        return HttpResponse('CREATE', status = 200)
+
     
 
 
