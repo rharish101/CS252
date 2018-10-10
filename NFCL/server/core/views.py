2ca23542a8741b13b1ac8747495873878b76f2c0
Ashish Kumar <ak.ashish2108@gmail.com>
2018-10-11 01:56:59 +0530

    updated nearby driver view

--- NFCL/server/core/views.py
+++ NFCL/server/core/views.py
@@ -6,6 +6,73 @@ from core.models import Driver
 import json, math
 from pyproj import Proj, transform
 
+def get_distance(a,b):
+    return math.sqrt( (a[1]-b[1])^2 + (a[0]-b[0])^2 )
+
+def nearbyDrivers(request):
+    data = json.loads(request.body.decode("utf-8"))
+    
+    try:
+        latitude = data['latitude']
+        longitude = data['longitude']
+    except:
+         return HttpResponse("ERROR", status=403)
+
+    x, y = transform(Proj(init='epsg:4326'), Proj(init='epsg:3857'), float(longitude), float(latitude))
+    # output (meters east of 0, meters north of 0):
+    #shortcuts for Web Mercator (EPSG 3857) and WGS 84 longitude and latitude (EPSG 4326). 
+    x_cordinate = int(x)
+    y_cordinate = int(y)
+    grid_size = 10000 # 10km * 10km
+    grid = ( int(x_cordinate/grid_size), int(y_cordinate/grid_size))
+    nearbygrids = [  
+                    (grid[0] , grid[1] ), 
+                    (grid[0] , grid[1] + 1 ),
+                    (grid[0] , grid[1] - 1),
+
+                    (grid[0] + 1, grid[1]),
+                    (grid[0] + 1, grid[1] + 1 ),
+                    (grid[0] + 1, grid[1] - 1 ),
+                    
+                    (grid[0] - 1, grid[1]),
+                    (grid[0] - 1, grid[1] + 1 ),
+                    (grid[0] - 1, grid[1] - 1 )
+                    
+                  ]
+    
+    near_by_grid_id = [str(hash(each)) for each in nearbygrids]
+
+    drivers = Driver.objects.filter(grid__in = near_by_grid_id)
+
+    drivers =  [ obj.to_dict() for obj in drivers ]
+
+    drivers =  [ {**each, 'distance':\
+                             get_distance(\
+                                (x_cordinate,y_cordinate), \
+                                (int(each['x_cordinate']), int(each[ 'y_cordinate']) ) \
+                              )\
+                        
+                } for each in drivers ]
+
+    drivers = sorted(drivers, key=lambda k: k['distance']) 
+
+    drivers = drivers[0:3]
+
+
+
+        
+    data = { 
+            'drivers' : drivers, 
+            'latitude' : str(latitude),
+            'longitude' : str(longitude),
+            'x_cordinate' : str(x_cordinate),
+            'y_cordinate' : str(y_cordinate)
+    }
+
+    response = JsonResponse(data)
+    response['Access-Control-Allow-Origin'] = '*'
+    response['Access-Control-Allow-Headers'] = '*'
+    return response
 
 
 @require_http_methods(["POST"])
