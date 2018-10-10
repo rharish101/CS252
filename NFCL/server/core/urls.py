0c6d9ab1b593150df9d66a00b8506b391fa4b39e
Ashish Kumar <ak.ashish2108@gmail.com>
2018-10-11 01:54:53 +0530

    updated the urls

--- NFCL/server/core/urls.py
+++ NFCL/server/core/urls.py
@@ -3,5 +3,6 @@ from core import views
 
 urlpatterns = [
     path('nearbyDrivers', views.nearbyDrivers, name='nearbyDrivers'),
-    path('updateDriver', views.updateDriver, name='upadteDriver'),
+    path('updateDriver', views.updateDriverDetail, name='upadteDriver'),
+     path('deleteDriver', views.deleteDriverDetail, name='deleteDriver'),
 ]
