c6999caf3e8126b097745177451890d5ae874fbe
Ashish Kumar <ak.ashish2108@gmail.com>
2018-10-11 01:54:15 +0530

    updated the model

--- NFCL/server/core/models.py
+++ NFCL/server/core/models.py
@@ -2,15 +2,28 @@ from django.db import models
 import uuid
 
 class Driver(models.Model):
+    mob_id = models.CharField(max_length=600,unique = True)
     name = models.CharField(max_length=300)
     mobile_no = models.IntegerField()
-    notification_id = models.CharField(max_length=600)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    running_status = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    x_cordinate = models.IntegerField()
+    y_cordinate = models.IntegerField()
+    grid = models.CharField(max_length=300)
     updated_at = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
-        return str(self.uuid) + "-" + str(self.name)
+        return str(self.mob_id) + "-" + str(self.name)
+
+    def to_dict(self):
+        return {
+            'name' : str(self.name),
+            'mobile_no' : str(self.mobile_no),
+            'latitude' : str(self.latitude),
+            'longitude' : str(self.longitude),
+            'x_cordinate' : str(self.x_cordinate),
+            'y_cordinate' : str(self.y_cordinate)
+            
+        }
  
