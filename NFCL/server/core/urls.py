10ec26e2cb3f5fa4e806fc97320ebcebdafa48b3
Ashish Kumar <ak.ashish2108@gmail.com>
2018-11-01 00:35:10 +0530

    added api for web client

--- NFCL/server/core/urls.py
+++ NFCL/server/core/urls.py
@@ -3,6 +3,7 @@ from core import views
 
 urlpatterns = [
     path('nearbyDrivers', views.nearbyDrivers, name='nearbyDrivers'),
+    path('nearbyDriversWeb', views.nearbyDriversWeb, name='nearbyDriversWeb'),
     path('updateDriver', views.updateDriverDetail, name='upadteDriver'),
      path('deleteDriver', views.deleteDriverDetail, name='deleteDriver'),
 ]
